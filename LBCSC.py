fe2fe365a6ed35cd42346b7e51dd2d49e4b5a292
Nuker <35502623+Nukerxz@users.noreply.github.com>
2022-10-30 01:59:58 +0200

    Update LBCSC.py
    big update avec rajout de la recherche automatique d'image sur google

--- LBCSC.py
+++ LBCSC.py
@@ -1,44 +1,19 @@
 #!/usr/bin/python3
-# https://www.leboncoin.fr/collection/2226549042.htm                article au hasard pour faire les tests
 
+import webbrowser
 import re
-import sys
-import requests
-import json
-from pprint import pprint
 from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 
 
-def mainmenu():
-    print("-----Le Bon Coin Scam Check-----")
-    print("Main Menu : -h for help")
-    print("1 : Full scan")
-    print("2 : Exit")
-    rep = input("")
-    if rep == "1":
-        fullscan()
-    if rep == "-h":
-        print("usage : \ntype 1 for full scan")
-        print("created by Nuker")
-        print("ATTENTION : Ce script a pour but de vous aider dans vos recherches, en aucun cas il est vecteur de "
-              "verite absolue et ne peut etre tenu pour "
-              "responsable si vous manquez une bonne affaire ou tout autre desagrement.")
-    if rep == "2":
-        quit()
-    if rep != "1" or rep != "-h" or rep != "2":
-        print("pas compris")
-        mainmenu()
-
-
-def fullscan():
-    url = input("paste your le bon coin url :")
+def scan():
+    # url = input("Merci de coller votre lien LE BON COIN ici : "
+    url = "https://www.leboncoin.fr/collection/2226549042.htm"               # article au hasard pour faire les tests
+    print("Scan en cours de la page {}".format(url))
     HEADLESS_MODE = True
-    JSON_FILE = "search-leboncoin.json"
-    SEARCH_TERM = "Thinkpad"
-    VERBOSE = True
 
     with sync_playwright() as p:
+        # permet de recuperer le code source de la page malgre datadome qui protege les requetes avec un cookie
         browser = p.firefox.launch(headless=HEADLESS_MODE, slow_mo=70)
         page = browser.new_page()
         page.goto(url)
@@ -47,27 +22,72 @@ def fullscan():
         html_source_code = page.content()
         soup = BeautifulSoup(html_source_code, 'html.parser')
 
-        # images and profile pic
+        # recuperer toutes les images de la page
         images = []
         for image in soup.find_all('img'):
             images.append((image.get('src')))
         images = set(images)
         images = list(images)
-        print(*images, sep="\n")
+        # print(*images, sep="\n") set a print le lien de chaque images
+        co = 0
+        for x in images:
+            co += 1
+        print("{} images ont été détectées, voulez-vous vérifier si ces images existent déjà en ligne ?".format(co))
+        imagerep = input("y or n ? :")
+        if imagerep == "y":
+            for image in images:
+                imagecheck(image)
+        else:
+            pass
+
+        # le titre de l'annonce
         title = soup.find("h1", {"data-qa-id": "adview_title"})
         title = str(title)
         title = re.sub(r'<.+?>', '', title)
+        print("Nom de l'article : ", title)
+
+        # nom/pseudo de l'annonceur
         name = soup.find("a", {"class": "_3k87M _3Hrjq _3Wx6b _2MFch _1hnil _35DXM _1-TTU _1GcfX _2DyF8 _3k00F"})
         name = str(name)
         name = re.sub(r'<.+?>', '', name)
-        desc = soup.find("p", {"class": "sc-iQNlJl bCyjVl"})
+        print("Nom du vendeur : ", name)
+
+        # description de l'article
+        desc = soup.find("p", {"class": "sc-hXRMBi lgCcJw"})
         desc = str(desc)
         desc = re.sub(r'<.+?>', '', desc)
-        print("Nom de l'article : ",title)
-        print("Nom du vendeur : ",name)
-        print("Description de l'article : ",desc)
-        # print(soup.prettify())              pour print l'entierete du html
-        browser.close()  # a mettre en fin de code
+        print("Description de l'article : ", desc)
+
+        # a mettre en fin de code
+        browser.close()
+        quit()
+
+
+def mainmenu():
+    print("-----Le Bon Coin Scam Check-----")
+    print("1 : Full scan")
+    print("2 : Help / Infos")
+    print("3 : Exit")
+    rep = input("")
+    if rep == "1":
+        scan()
+    if rep == "2":
+        print("created by Nuker")
+        print("Ce script a pour but de vous aider dans vos recherches en automatisant certaines actions, merci "
+              "de faire remonter toutes remarques, suggestions ou bugs directement sur le github disponible : "
+              "https://github.com/Nukerxz/LBCSC")
+        print("Utilisation :"
+              "Tapez 1 puis coller l'url de l'article LE BON COIN que vous voulez scanner. "
+              "L'url doit avoir ce format : https://www.leboncoin.fr/collection/0000000000.htm")
+
+    if rep == "3":
+        quit()
+    else:
+        mainmenu()
+
+
+def imagecheck(imglink):
+    webbrowser.open('http://www.google.com/searchbyimage?image_url={}'.format(imglink))
 
 
 mainmenu()
